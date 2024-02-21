ec9edf0c68eac9ebcdb95345aec24773ce3b865d
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-02-21 19:34:11 +0000

    Add XML Documentation to class Program

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// The main class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The main function where all the code will be written
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
