9a460faeed57cb45d749ed42782489532b198747
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-02-21 21:45:12 +0000

    Added VariousTypes project and multiple integral variables to its Program class

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1 = 1;
+            long long1 = 2L;
+            ulong ulong1 = 3UL;
+            Console.WriteLine(int1);
+            Console.WriteLine(long1);
+            Console.WriteLine(ulong1);
+        }
+    }
+}
