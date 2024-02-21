b2d0f6947326ecb5b6e145c99002d86760330d9f
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-02-21 23:03:49 +0000

    Reading user input and parsing it into a float variable, project AskForInput

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -7,8 +7,14 @@ namespace AskForInput
         static void Main(string[] args)
         {
             //Asking for user input and parsing it into an int variable
+            Console.WriteLine("Please insert an int");
             string userInput = Console.ReadLine();
             int userInt = int.Parse(userInput);
+
+            //Asking for user input and parsing it into an int variable
+            Console.WriteLine("Please insert a float");
+            string userInput2 = Console.ReadLine();
+            float userFloat = int.Parse(userInput2);
         }
     }
 }
