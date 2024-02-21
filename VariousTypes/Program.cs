fcce74166cac627d8525827e7cf86b81aee9c973
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-02-21 21:55:17 +0000

    Added multiple char variables to the Program class in VariousTypes project

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,12 +6,21 @@ namespace VariousTypes
     {
         static void Main(string[] args)
         {
+            //Initializing multiple integral variables and printing them
             int int1 = 1;
             long long1 = 2L;
             ulong ulong1 = 3UL;
             Console.WriteLine(int1);
             Console.WriteLine(long1);
             Console.WriteLine(ulong1);
+
+            //Initializing multiple char variables and printing them
+            char char1 = '\u006F';
+            char char2 = '\u006C';
+            char char3 = '\u0061';
+            Console.WriteLine(char1);
+            Console.WriteLine(char2);
+            Console.WriteLine(char3);
         }
     }
 }
