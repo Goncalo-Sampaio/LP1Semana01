b4ff79ea7148773e05416aeb7cd0e4f626897e7f
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-02-21 22:15:14 +0000

    Added 2 string variables to the Program class on ManyStrings project

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,7 +6,11 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Initializing multiple string variables and printing them
+            string s1 = "A tab \t and a right arrow \u2192";
+            string s2 = "A new line \n and a star \u2605";
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
         }
     }
 }
