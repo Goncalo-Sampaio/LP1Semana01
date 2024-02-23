3d008a5a53d8acee992585237d71398f88f10d36
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-02-23 16:25:17 +0000

    Added user input and parsed it to ints, DecimalHexa project

--- DecimalHexa/Program.cs
+++ DecimalHexa/Program.cs
@@ -6,7 +6,18 @@ namespace DecimalHexa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Asking for user input
+            Console.WriteLine("Insere 4 números inteiros:");
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string input3 = Console.ReadLine();
+            string input4 = Console.ReadLine();
+
+            //Parsing each of the inputs to ints
+            int int1 = int.Parse(input1);
+            int int2 = int.Parse(input2);
+            int int3 = int.Parse(input3);
+            int int4 = int.Parse(input4);
         }
     }
 }
